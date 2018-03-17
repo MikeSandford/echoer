43ad581d967ec00e3fde78dd2a02b31861b67503
Mike Sandford <sandford@ufl.edu>
2018-03-17 13:57:59 -0500

    Argparse version

--- start.py
+++ start.py
@@ -1,12 +1,22 @@
 
 import sys
 import pyttsx3
+import argparse
+
+
+def echo_stuff(stuff):
+    engine = pyttsx3.init()
+    engine.say(stuff)
+    engine.runAndWait()
 
 if __name__ == "__main__":
 
-    words = sys.argv[1:]
-    word_string = " ".join(words)
+    parser = argparse.ArgumentParser(description='Say something out loud')
+    parser.add_argument('words', type=str, nargs='+',
+                        help='Words to be spoken')
 
-    engine = pyttsx3.init()
-    engine.say(word_string)
-    engine.runAndWait()
+    args = parser.parse_args()
+    print(args.words)
+    word_string = " ".join(args.words)
+    print(word_string)
+    echo_stuff(word_string)
