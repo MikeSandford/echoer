ba4f4a3d37456e94732ae48366aa84fc00109639
Mike Sandford <sandford@ufl.edu>
2018-03-19 20:43:30 -0500

    CLI callable

--- echoer/start.py
+++ echoer/start.py
@@ -9,14 +9,15 @@ def echo_stuff(stuff):
     engine.say(stuff)
     engine.runAndWait()
 
-if __name__ == "__main__":
-
+def main():
     parser = argparse.ArgumentParser(description='Say something out loud')
     parser.add_argument('words', type=str, nargs='+',
                         help='Words to be spoken')
 
     args = parser.parse_args()
-    print(args.words)
     word_string = " ".join(args.words)
-    print(word_string)
     echo_stuff(word_string)
+
+if __name__ == "__main__":
+
+    main()
--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@
 from distutils.core import setup
 
 setup(name='Echoer',
-      version='1.2',
-      description='Say things out loud',
-      author='Mike Sandford',
-      author_email='mike.sandford@arundo.com',
-      url='https://github.com/MikeSandfordArundo/echoer.git',
-      packages=['echoer']
-     )
+    version='1.3',
+    description='Say things out loud',
+    author='Mike Sandford',
+    author_email='mike.sandford@arundo.com',
+    url='https://github.com/MikeSandfordArundo/echoer.git',
+    packages=['echoer'],
+    entry_points={
+        'console_scripts': [
+            'echoer=echoer.start:main',
+        ],
+    },
+    )
