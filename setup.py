eb4153154a2c6e4ce92e1c1a28a0b2412205ab59
Mike Sandford <sandford@ufl.edu>
2018-03-19 20:28:02 -0500

    Add setup.py, not a great package

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Echoer',
+      version='1.1',
+      description='Say things out loud',
+      author='Mike Sandford',
+      author_email='mike.sandford@arundo.com',
+      url='https://github.com/MikeSandfordArundo/echoer.git',
+      py_modules=['start.py']
+     )
